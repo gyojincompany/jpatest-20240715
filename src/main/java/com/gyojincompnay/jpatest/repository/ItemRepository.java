1216b62456d745a5da28516b9d96cb07277dcec3
gyojincompany <luckyred@gmail.com>
2024-07-15 16:18:35 +0900

    조회 테스트 추가

--- src/main/java/com/gyojincompnay/jpatest/repository/ItemRepository.java
+++ src/main/java/com/gyojincompnay/jpatest/repository/ItemRepository.java
@@ -3,7 +3,12 @@ package com.gyojincompnay.jpatest.repository;
 import com.gyojincompnay.jpatest.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
+    List<Item> findByItemNm(String itemNm);
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+    List<Item> findByPriceLessThanEqual(int price);
 
 }
--- src/test/java/com/gyojincompnay/jpatest/ItemRepositoryTest.java
+++ src/test/java/com/gyojincompnay/jpatest/ItemRepositoryTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 public class ItemRepositoryTest {
@@ -18,17 +20,48 @@ public class ItemRepositoryTest {
     @Test
     @DisplayName("상품 저장 테스트")
     public void createItemTest() {
-        Item item = new Item();
-        item.setItemNm("테스트 상품");
-        item.setPrice(10000);
-        item.setItemDetail("테스트 상품 상세 설명");
-        item.setStockNumber(100);
-        item.setRegTime(LocalDateTime.now());//현재 시간으로 삽입
-        item.setUpdateTime(LocalDateTime.now());
+        for(int i=1;i<=10;i++) {
+            Item item = new Item();
+            item.setItemNm("테스트 상품" + i);
+            item.setPrice(10000+i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());//현재 시간으로 삽입
+            item.setUpdateTime(LocalDateTime.now());
 
-        Item savedItem = itemRepository.save(item); //DB에 insert문이 실행되는 것과 같은 기능 실행
-        System.out.println(savedItem.toString());
+            Item savedItem = itemRepository.save(item); //DB에 insert문이 실행되는 것과 같은 기능 실행
+            System.out.println(savedItem.toString());
+        }
+    }
 
+    @Test
+    @DisplayName("상품명 조회 테스트")
+    public void findByItemNmTest() {
+        List<Item> itemList = itemRepository.findByItemNm("테스트 상품");
+
+        for(Item item:itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("상품가격이 10,000원 이하 상품 조회 테스트")
+    public void findByPriceLessThanEqualTest() {
+        List<Item> itemList = itemRepository.findByPriceLessThanEqual(10000);
+
+        for(Item item:itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("상품명 또는 상품상세설명 조회 테스트")
+    public void findByItemOrItemDetailTest() {
+        List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품", "테스트 상품 상세 설명7");
+
+        for(Item item:itemList) {
+            System.out.println(item.toString());
+        }
     }
 
 }
